4cc007060ce456aed3fa9ff16010b2d6b44b46bb
Aleksandr Zuev <zuev08@gmail.com>
2024-05-04 15:52:41 +0200

    Move bubbletea model into separate file

--- main.go
+++ main.go
@@ -7,27 +7,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct{}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		}
-	}
-	return m, nil
-}
-
-func (m model) View() string {
-	return "Hello, World!"
-}
-
 func main() {
 	m := model{}
 	p := tea.NewProgram(m)
