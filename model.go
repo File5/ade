bd69b1ed96e06a0bbaee1ad47bce715b914ad818
Aleksandr Zuev <zuev08@gmail.com>
2024-05-04 16:54:04 +0200

    Try lipgloss styles

--- model.go
+++ model.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
+
+var lightStatus = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("0")).
+	Background(lipgloss.Color("15"))
 
 type model struct {
 	loading bool
 	width   int
 	height  int
+	canvas  string
 }
 
 func newModel() model {
 	return model{loading: true}
+}
+
+func (m *model) resize(w, h int) {
+	m.width = w
+	m.height = h
+
+	ch := h - 2
+	canvas := make([]string, ch)
+	for i := 0; i < ch; i++ {
+		canvas[i] = ""
+	}
+	m.canvas = strings.Join(canvas, "\n")
+
+	lightStatus.Width(w)
 }
 
 func (m model) Init() tea.Cmd {
@@ -25,8 +46,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tea.WindowSizeMsg:
 		m.loading = false
-		m.width = msg.Width
-		m.height = msg.Height
+		m.resize(msg.Width, msg.Height)
 
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -41,5 +61,7 @@ func (m model) View() string {
 	if m.loading {
 		return "loading..."
 	}
-	return fmt.Sprintf("WindowSize[w=%d, h=%d]", m.width, m.height)
+	status := lightStatus.Render("[status]")
+	cmdline := "[cmd]"
+	return lipgloss.JoinVertical(lipgloss.Top, m.canvas, status, cmdline)
 }
