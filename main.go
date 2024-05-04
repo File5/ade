02c9bcf54c9f985076fd732ced29abd22370381a
Aleksandr Zuev <zuev08@gmail.com>
2024-05-04 15:43:07 +0200

    Example go bubbletea cmd-line app

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return "Hello, World!"
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	m := model{}
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
